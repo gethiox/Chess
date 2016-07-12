7972c9de9a6c879b5e7fb3b79fab8b710013fa29
Sławomir Kur <slawomir.kur@freshmail.pl>
2016-07-12 15:56:14 +0200

    wzbogacenie helpa

--- chess.py
+++ chess.py
@@ -1169,15 +1169,22 @@ def parse_args():
     parser.add_argument('--help', action='help', help='Show Help')
 
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--alone', dest='forever_alone', action='store_true', help='Human vs Human')
-    group.add_argument('-n', '--normal', dest='human', action='store_true', help='Hunam vs Computer')
-    group.add_argument('-a', '--auto', dest='auto', action='store_true', help='Computer vs Computer')
+    group.add_argument('-f', '--alone', dest='forever_alone', action='store_true',
+                       help='Human vs Human')
+    group.add_argument('-n', '--normal', dest='human', action='store_true',
+                       help='Hunam vs Computer (chess engine required)')
+    group.add_argument('-a', '--auto', dest='auto', action='store_true',
+                       help='Computer vs Computer (chess engine required)')
 
-    group.add_argument('-h', '--host', dest='host', metavar='<ip>', default=None, help='Host a network game')
-    group.add_argument('-j', '--join', dest='join', metavar='<ip>', default=None, help='Join a network game')
+    group.add_argument('-h', '--host', dest='host', metavar='<ip>', default=None,
+                       help='Host a network game')
+    group.add_argument('-j', '--join', dest='join', metavar='<ip>', default=None,
+                       help='Join a network game')
 
-    parser.add_argument('-p', '--port', dest='port', metavar='<port>', type=int, default=5678, help='Destination Port')
-    parser.add_argument('-s', '--stock', dest='stock', metavar='./stockfish', type=str, default='./src/helpers/stockfish', help='Path to the Stockfish binary')
+    parser.add_argument('-p', '--port', dest='port', metavar='<port>', type=int, default=5678,
+                        help='Destination Port')
+    parser.add_argument('-s', '--stock', dest='stock', metavar='./stockfish', type=str, default='./src/helpers/stockfish',
+                        help='Path to the Stockfish binary')
 
     args = parser.parse_args()
     return args

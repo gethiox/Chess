5215f530448c81d9b614365fd8c2e7fa78ff01f0
Gethiox <gethiox@gmail.com>
2017-09-21 00:08:52 +0200

    created Position object

--- src/domain/pieces.py
+++ src/domain/pieces.py
@@ -1,7 +1,32 @@
-from src.app.pieces import Position
+from typing import Union, Tuple
 
 white = 0
 black = 1
+
+ranks = '12345678'
+files = 'abcdefgh'
+
+
+class Position:
+    def __init__(self, pos: Union[str, Tuple[int]]):
+        if isinstance(pos, tuple):
+            if len(pos) != 2:
+                raise ValueError('Position should be given as tuple/list with only two ints')
+            self.pos = [pos[0], pos[1]]
+        elif isinstance(pos, str):
+            if len(pos) != 2:
+                raise ValueError('Position should be given as two letter coordinates (file, rank)')
+            self.pos = [files.index(pos[0]), ranks.index(pos[1])]
+
+    def __repr__(self):
+        return '<Position: %s>' % str(self)
+
+    def __str__(self):
+        return '%s%s' % (files[self.pos[0]], ranks[self.pos[1]])
+
+    def __iter__(self):
+        for coordinate in self.pos:
+            yield coordinate
 
 
 class Piece:

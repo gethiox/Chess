63b4db905cf274d50d1a08c539e0dfa24265a067
Gethiox <gethiox@gmail.com>
2017-09-21 00:10:10 +0200

    board from_fen method

--- src/app/game.py
+++ src/app/game.py
@@ -1,6 +1,10 @@
+from string import digits
 from typing import List, Optional
 
-from src.domain.pieces import Piece
+from app.cli_board import tiny_rendererer
+from app.pieces import from_str
+from domain.pieces import Piece
+from domain.pieces import Position
 
 
 class Board:
@@ -16,31 +20,39 @@ class Board:
         else:
             self.pieces = []
 
-    def array(self) -> List[List[Optional[Piece]]]:
-        """
-        Dynamically generated two-dimensional board array
-        :return: 8x8 board array
-        """
-        arr = [[None for _ in range(8)] for _ in range(8)]
+    def render(self):
+        tiny_rendererer(self._array())
 
-        for piece in self.pieces:
-            x, y = tuple(piece.position)
-            arr[x][y] = piece
+    def set_fen(self, board_fen: str):
+        pieces = []
 
-        return arr
+        y = 7
+        for rank in board_fen.split('/'):
+            x = 0
+            for piece in rank:
+                if piece not in digits:
+                    pieces.append(from_str(piece, Position((x, y))))
+                    x += 1
+                else:
+                    for i in range(int(piece)):
+                        x += 1
+            y -= 1
 
-    def fen(self) -> str:
+        self.pieces = pieces
+
+    def get_fen(self) -> str:
         """
         :return: FEN representation of board state
         """
-        arr = self.array()
+        arr = self._array()
         board_fen = ''
 
         # Here we go into C programming style
 
-        x, y = 0, 7
         no_pieces_counter = 0
+        y = 7
         while y >= 0:
+            x = 0
             while x <= 7:
                 piece = arr[x][y]
                 if piece:
@@ -56,19 +68,35 @@ class Board:
                 no_pieces_counter = 0
             if y > 0:
                 board_fen += '/'
-            x = 0
             y -= 1
 
         # Pretty easy to understand
 
         return board_fen
 
+    def _array(self) -> List[List[Optional[Piece]]]:
+        """
+        Dynamically generated two-dimensional board array
+        :return: 8x8 board array
+        """
+        arr = [[None for _ in range(8)] for _ in range(8)]
+
+        for piece in self.pieces:
+            x, y = tuple(piece.position)
+            arr[x][y] = piece
+
+        return arr
+
     def __repr__(self):
-        return '<Board: %s>' % self.fen()
+        return '<Board: %s>' % self.get_fen()
 
     def __str__(self):
-        return self.fen()
+        return self.get_fen()
 
 
 class Chess:
-    pass  # TODO
+    def __init__(self):
+        self.board = Board()
+
+    def new_game(self):
+        self.board.set_fen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')

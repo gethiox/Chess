d9271f8c5e67393c501ea2eef0784ff7996c1c82
Gethiox <gethiox@gmail.com>
2017-09-25 22:13:44 +0200

    tiny refactor, added piece points information

--- src/app/pieces.py
+++ src/app/pieces.py
@@ -1,37 +1,59 @@
-from domain.pieces import Piece, Position, white, black
+from math import inf as infinity
+
+from domain.pieces import Piece, White, Black
 
 
 class King(Piece):
     name = 'King'
     char = 'k'
+    points = infinity
 
 
 class Queen(Piece):
     name = 'Queen'
     char = 'q'
+    points = 9
 
 
 class Rook(Piece):
     name = 'Rook'
     char = 'r'
+    points = 5
 
 
 class Bishop(Piece):
     name = 'Bishop'
     char = 'b'
+    points = 3
 
 
 class Knight(Piece):
     name = 'Knight'
     char = 'n'
+    points = 3
 
 
 class Pawn(Piece):
     name = 'Pawn'
     char = 'p'
+    points = 1
 
 
-def from_str(piece: str, position: Position) -> Piece:
-    for real_piece in [King, Queen, Rook, Bishop, Knight, Pawn]:
-        if real_piece.char == piece.lower():
-            return real_piece(black if piece.islower() else white, position)
+str_map = {
+    'k': King,
+    'q': Queen,
+    'r': Rook,
+    'b': Bishop,
+    'n': Knight,
+    'p': Pawn,
+}
+
+
+def from_str(piece: str) -> Piece:
+    """
+    Method designed to return Piece object from given one-letter string.
+    Needed only for processing FEN board-state format.
+    """
+    if piece.lower() not in ['k', 'q', 'r', 'b', 'n', 'p']:
+        raise ValueError('"%s" not defined as Chess piece.', piece)
+    return str_map[piece](White) if piece.isupper() else str_map[piece](Black)

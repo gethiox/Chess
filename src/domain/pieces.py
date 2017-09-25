1fd430f2becd8fd5711db19407cee6e6203f0e8e
Gethiox <gethiox@gmail.com>
2017-09-25 22:09:02 +0200

    I don't think using __name__ statement is a good idea here, however

--- src/domain/pieces.py
+++ src/domain/pieces.py
@@ -6,13 +6,13 @@ files = 'abcdefgh'
 
 class Side:
     def __init__(self, name: str):
-        self.__name__ = name
+        self.name = name
 
     def __repr__(self):
-        return self.__name__
+        return self.name
 
     def __str__(self):
-        return self.__name__
+        return self.name
 
 
 White = Side("White")

bbd8ae92767a3320763f9f2afc21f9cc33b8b982
Gethiox <gethiox@gmail.com>
2017-09-25 22:01:02 +0200

    small refactor, create player-side objects

--- src/domain/pieces.py
+++ src/domain/pieces.py
@@ -1,15 +1,42 @@
-from typing import Union, Tuple
-
-white = 0
-black = 1
+from typing import Union, Tuple, List
 
 ranks = '12345678'
 files = 'abcdefgh'
 
 
+class Side:
+    def __init__(self, name: str):
+        self.__name__ = name
+
+    def __repr__(self):
+        return self.__name__
+
+    def __str__(self):
+        return self.__name__
+
+
+White = Side("White")
+Black = Side("Black")
+
+
+class Piece:
+    name = 'Piece'
+    char = 'p'
+    points = 1
+
+    def __init__(self, side: Side):
+        self.side = side
+
+    def __repr__(self):
+        return '%s %s' % (self.side, self.name)
+
+    def __str__(self):
+        return self.char.upper() if self.side == White else self.char.lower()
+
+
 class Position:
-    def __init__(self, pos: Union[str, Tuple[int]]):
-        if isinstance(pos, tuple):
+    def __init__(self, pos: Union[str, Tuple[int], List[int]]):
+        if isinstance(pos, tuple) or isinstance(pos, list):
             if len(pos) != 2:
                 raise ValueError('Position should be given as tuple/list with only two ints')
             self.pos = [pos[0], pos[1]]
@@ -19,32 +46,12 @@ class Position:
             self.pos = [files.index(pos[0]), ranks.index(pos[1])]
 
     def __repr__(self):
-        return '<Position: %s>' % str(self)
+        return 'Position: %s' % str(self)
 
     def __str__(self):
-        return '%s%s' % (files[self.pos[0]], ranks[self.pos[1]])
+        return '%s%s' % (files[self.pos[0]],
+                         ranks[self.pos[1]])
 
     def __iter__(self):
         for coordinate in self.pos:
             yield coordinate
-
-
-class Piece:
-    name = 'Piece'
-    char = 'p'
-    points = 1
-
-    def __init__(self, side: int):
-        self.side = side
-
-    def __repr__(self):
-        if self.side == white:
-            return '<White %s>' % self.name
-        elif self.side == black:
-            return '<Black %s>' % self.name
-
-    def __str__(self):
-        if self.side == white:
-            return self.char.upper()
-        elif self.side == black:
-            return self.char.lower()

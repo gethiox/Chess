9ebae4fa068fb8446bedb7efc16f26ac416ed569
Gethiox <gethiox@gmail.com>
2017-09-26 22:50:36 +0200

    object properties as readonly - experimenting with @property

--- src/domain/pieces.py
+++ src/domain/pieces.py
@@ -6,13 +6,17 @@ files = 'abcdefgh'
 
 class Side:
     def __init__(self, name: str):
-        self.name = name
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
 
     def __repr__(self):
-        return self.name
+        return self.__name
 
     def __str__(self):
-        return self.name
+        return self.__name
 
 
 White = Side("White")
@@ -28,38 +32,47 @@ class Piece:
     points = 1
 
     def __init__(self, side: Side):
-        self.side = side
+        self.__side = side
+
+    @property
+    def side(self):
+        return self.__side
 
     def __repr__(self):
-        return '%s %s' % (self.side, self.name)
+        return '%s %s' % (self.__side, self.name)
 
     def __str__(self):
-        return self.char.upper() if self.side == White else self.char.lower()
+        return self.char.upper() if self.__side == White else self.char.lower()
 
 
 class Position:
     """
     tuple with two board coordinates is too simple of course
     """
+
     def __init__(self, pos: Union[str, Tuple[int], List[int]]):
         if isinstance(pos, tuple) or isinstance(pos, list):
             if len(pos) != 2:
                 raise ValueError('Position should be given as tuple/list with only two ints')
-            self.pos = [pos[0], pos[1]]
+            self.__pos = [pos[0], pos[1]]
         elif isinstance(pos, str):
             if len(pos) != 2:
                 raise ValueError('Position should be given as two letter coordinates (file, rank)')
-            self.pos = [files.index(pos[0]), ranks.index(pos[1])]
+            self.__pos = [files.index(pos[0]), ranks.index(pos[1])]
+
+    @property
+    def pos(self):
+        return self.__pos
 
     def __repr__(self):
-        return 'Position: %s' % str(self)
+        return 'Position: %s' % self
 
     def __str__(self):
-        return '%s%s' % (files[self.pos[0]],
-                         ranks[self.pos[1]])
+        return '%s%s' % (files[self.__pos[0]],
+                         ranks[self.__pos[1]])
 
     def __iter__(self) -> Iterator[int]:
-        for coordinate in self.pos:
+        for coordinate in self.__pos:
             yield coordinate
 
 
@@ -67,19 +80,32 @@ class Move:
     """
     Two Position aggregator with optional pawn promotion information
     """
+
     def __init__(self, a: Position, b: Position, promotion: Optional[Piece]):
-        self.a = a
-        self.b = b
-        self.promotion = promotion
+        self.__a = a
+        self.__b = b
+        self.__promotion = promotion
+
+    @property
+    def a(self):
+        return self.__a
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def promotion(self):
+        return self.__promotion
 
     def __repr__(self):
-        if self.promotion:
-            return 'Move: %s to %s with promotion to %s' % (self.a, self.b, self.promotion.name)
+        if self.__promotion:
+            return 'Move: %s to %s with promotion to %s' % (self.__a, self.__b, self.__promotion.name)
         else:
-            return 'Move: %s to %s' % (self.a, self.b)
+            return 'Move: %s to %s' % (self.__a, self.__b)
 
     def __str__(self):
-        if self.promotion:
-            return '%s%s%s' % (self.a, self.b, self.promotion.char)
+        if self.__promotion:
+            return '%s%s%s' % (self.__a, self.__b, self.__promotion.char)
         else:
-            return '%s%s' % (self.a, self.b)
+            return '%s%s' % (self.__a, self.__b)

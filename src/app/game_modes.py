362c0a979e63458e9c46bced84895dd14718b1d3
Gethiox <gethiox@gmail.com>
2017-09-26 23:18:16 +0200

    init game modes to support

---
+++ src/app/game_modes.py
@@ -0,0 +1,53 @@
+# see more: https://en.wikipedia.org/wiki/List_of_chess_variants
+
+
+class GameMode:
+    pass
+
+
+class Normal(GameMode):
+    pass
+
+
+class Chess960(GameMode):
+    pass
+
+
+class PreChess(GameMode):
+    """Could be interesting to implement"""
+    pass
+
+
+class UpsideDown(GameMode):
+    pass
+
+
+class Double(GameMode):
+    """Could be interesting to implement as well"""
+    pass
+
+
+class Crazyhouse(GameMode):
+    pass
+
+
+class KingOfTheHill(GameMode):
+    pass
+
+
+class ThreeCheck(GameMode):
+    pass
+
+
+class Horde(GameMode):
+    pass
+
+
+class Antichess(GameMode):
+    """Just because LiChess supports it"""
+    pass
+
+
+class RacingKings(GameMode):
+    """Like above"""
+    pass

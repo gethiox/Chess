f10155fb54fae2a1b6cd91402276b35c8f16522e
Gethiox <gethiox@gmail.com>
2017-09-28 23:25:42 +0200

    first attempt to build game mode schema

--- src/app/game_modes.py
+++ src/app/game_modes.py
@@ -1,8 +1,36 @@
 # see more: https://en.wikipedia.org/wiki/List_of_chess_variants
 
+from abc import abstractmethod
+from time import time
+
+from domain.pieces import Side
+
 
 class GameMode:
-    pass
+    """
+    Generic Game Mode class
+    """
+    start_time = None
+
+    @property
+    @abstractmethod
+    def on_move(self) -> Side:
+        """ REQUIRED
+        :return: Side
+        """
+        return NotImplemented
+
+    def start_game(self):
+        self.start_time = time()
+
+    @abstractmethod
+    def game_state(self):
+        """ REQUIRED
+        method return game state which depends on specific rules for every game mode.
+        Look into "Normal" game mode class for inspirations (if is even implemented right now)
+        :return: not_yet_started OR on_move_side OR winner_side
+        """
+        raise NotImplemented
 
 
 class Normal(GameMode):
@@ -10,20 +38,6 @@ class Normal(GameMode):
 
 
 class Chess960(GameMode):
-    pass
-
-
-class PreChess(GameMode):
-    """Could be interesting to implement"""
-    pass
-
-
-class UpsideDown(GameMode):
-    pass
-
-
-class Double(GameMode):
-    """Could be interesting to implement as well"""
     pass
 
 
@@ -40,6 +54,20 @@ class ThreeCheck(GameMode):
 
 
 class Horde(GameMode):
+    pass
+
+
+class PreChess(GameMode):
+    """Could be interesting to implement"""
+    pass
+
+
+class UpsideDown(GameMode):
+    pass
+
+
+class Double(GameMode):
+    """Could be interesting to implement as well"""
     pass
 
 

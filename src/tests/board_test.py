68aa899358305218d60fe7e45154c8662c456a74
Gethiox <gethiox@gmail.com>
2017-10-04 22:47:38 +0200

    get_fen() more readable

--- src/tests/board_test.py
+++ src/tests/board_test.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app.game import Board
-from app.pieces import King, Queen, Rook, Bishop, Knight, Pawn
+from app.pieces import King, Queen, Rook
 from domain.pieces import White, Black, Position
 
 
@@ -10,17 +10,17 @@ class BoardTestCase(TestCase):
         board = Board()
         board._put_piece(piece=King(White),
                          position=Position('e1'))
-        self.assertEqual(str(board), '8/8/8/8/8/8/8/4K3')
+        self.assertEqual(board.get_fen(), '8/8/8/8/8/8/8/4K3')
 
         board._put_piece(piece=King(Black),
                          position=Position('e8'))
-        self.assertEqual(str(board), '4k3/8/8/8/8/8/8/4K3')
+        self.assertEqual(board.get_fen(), '4k3/8/8/8/8/8/8/4K3')
 
         board._put_piece(piece=Queen(White),
                          position=Position('d1'))
         board._put_piece(piece=Queen(Black),
                          position=Position('d8'))
-        self.assertEqual(str(board), '3qk3/8/8/8/8/8/8/3QK3')
+        self.assertEqual(board.get_fen(), '3qk3/8/8/8/8/8/8/3QK3')
 
         board._put_piece(piece=Rook(White),
                          position=Position('a1'))
@@ -31,9 +31,7 @@ class BoardTestCase(TestCase):
         board._put_piece(piece=Rook(Black),
                          position=Position('h8'))
 
-        self.assertEqual(str(board), 'r2qk2r/8/8/8/8/8/8/R2QK2R')
-
-
+        self.assertEqual(board.get_fen(), 'r2qk2r/8/8/8/8/8/8/R2QK2R')
 
     def write_fenstring_test(self):
         board = Board()
@@ -44,5 +42,3 @@ class BoardTestCase(TestCase):
         board.set_fen('4k3/8/8/8/8/8/8/4K3')
         self.assertEqual(board._get_piece(position=Position('e1')), King(White))
         self.assertEqual(board._get_piece(position=Position('e8')), King(Black))
-
-

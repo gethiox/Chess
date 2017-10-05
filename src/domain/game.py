60ca0f4cbc85d10f87fe600096bc16b7fd5f8922
Gethiox <gethiox@gmail.com>
2017-10-05 21:57:42 +0200

    some GameMode developing

--- src/domain/game.py
+++ src/domain/game.py
@@ -1,17 +1,21 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
+from typing import Tuple
+from functools import lru_cache
 
-from domain.pieces import Move
+from domain.pieces import Move, Piece, Side
 
 
-class GameMoode(ABCMeta):
-    def init_board_state(cls) -> str:
+class GameMode(metaclass=ABCMeta):
+    @abstractmethod
+    def init_board_state(self) -> str:
         """
         Sets Board init state for this game mode
         :return FEN strong of part with board only
         """
         pass
 
-    def assert_move(cls, move: Move) -> bool:
+    @abstractmethod
+    def assert_move(self, move: Move) -> bool:
         """
         Assert if given move in current game state and game mode is legal
         :param move: Move type
@@ -19,4 +23,32 @@ class GameMoode(ABCMeta):
         """
         pass
 
-    # TODO: TODO
+    @property
+    @abstractmethod
+    def pieces(self) -> Tuple[Piece]:
+        """
+        :return: Tuple of Pieces supported with implemented GameMode
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def sides(self) -> Tuple[Side]:
+        """
+        :return: tuple of players sides
+        """
+        pass
+
+    @lru_cache(maxsize=1)
+    def __pieces_dict(self):
+        d = {}
+        for piece in self.pieces:
+            d.update({piece.char: piece})
+        return d
+
+    @lru_cache(maxsize=1)
+    def __sides_dict(self):
+        d = {}
+        for side in self.sides:
+            d.update({side.char: side})
+        return d

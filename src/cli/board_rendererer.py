e928bc790a6d4a7350b7c43f7eb3383158844556
Gethiox <gethiox@gmail.com>
2017-11-05 19:17:10 +0100

    added bigger cli renderer

--- src/cli/board_rendererer.py
+++ src/cli/board_rendererer.py
@@ -21,3 +21,28 @@ def tiny(board: StandardBoard):
             board_str += '\n'
         rank_counter -= 1
     print(board_str)
+
+
+def normal(board: StandardBoard):
+    # TODO: add support for variable board size
+    board_str = '+---+---+---+---+---+---+---+---+\n'
+
+    rank_counter = 7
+    while rank_counter >= 0:
+        board_str += '| '
+        file_counter = 0
+        while file_counter <= 7:
+            piece = board.array[file_counter][rank_counter]
+            if piece:
+                board_str += str(piece)
+            else:
+                board_str += ' '
+            if file_counter < 7:
+                board_str += ' | '
+            file_counter += 1
+        board_str += ' |'
+        if rank_counter > 0:
+            board_str += '\n+---+---+---+---+---+---+---+---+\n'
+        rank_counter -= 1
+    board_str += '\n+---+---+---+---+---+---+---+---+'
+    print(board_str)

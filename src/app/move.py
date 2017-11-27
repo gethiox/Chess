249925a75d4545f062b0b49c24fe1141a400c593
gethiox <gethiox@gmail.com>
2017-11-27 19:21:04 +0100

    implement StandardMove.from_str

--- src/app/move.py
+++ src/app/move.py
@@ -1,15 +1,25 @@
-from typing import TYPE_CHECKING
-
+from app.pieces import from_str
+from app.position import StandardPosition
 from interface.move import Move
-
-if TYPE_CHECKING:
-    from app.position import StandardPosition
 
 
 class StandardMove(Move):
     """
     Two Position aggregator with optional pawn promotion information
     """
+
+    @classmethod
+    def from_str(cls, move_str: str):
+        if len(move_str) == 4:
+            return cls(StandardPosition.from_str(move_str[0:2]),
+                       StandardPosition.from_str(move_str[2:4]))
+
+        elif len(move_str) == 5:
+            return cls(StandardPosition.from_str(move_str[0:2]),
+                       StandardPosition.from_str(move_str[2:4]),
+                       from_str(move_str[4], initialized=False))
+        else:
+            raise ValueError('Primitive move is described as 4-5 length string (without or with promotion)')
 
     @property
     def source(self) -> 'StandardPosition':

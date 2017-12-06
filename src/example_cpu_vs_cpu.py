e4021a3b8cb8f2e64d446e4c5554e320bfa077b5
Gethiox <gethiox@gmail.com>
2017-12-06 23:42:39 +0100

    cpu_vs_cpu example update

--- src/example_cpu_vs_cpu.py
+++ src/example_cpu_vs_cpu.py
@@ -12,15 +12,18 @@ from interface.game import Game
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument(dest='engine_path', type=str, nargs='?', help='path of UCI chess engine')
+    parser.add_argument('-s', '--silent', dest='silent', action='store_true', help='path of UCI chess engine')
     return parser.parse_args()
 
 
 def print_state(game):
     board_str = board_rendererer.normal(game.board)
+    fen = str(game.variant)
     data = "On move: {on_move!s:5s}, Available moves: {moves:s}".format(
         on_move=game.variant.on_move,
         moves='disabled',  # len(game.variant.all_available_moves())  # warning: inefficient!
     )
+    print(fen)
     print(board_str)
     print(data)
     print()
@@ -40,32 +43,41 @@ if __name__ == "__main__":
 
     # engine is created for each side to prevent "self-seeing" moves, it simulate two different players instead of one
     engine1 = EngineHandler(args.engine_path, threads=1, ponder=False)
-    engine2 = EngineHandler(args.engine_path, threads=1, ponder=True)
+    engine2 = EngineHandler(args.engine_path, threads=1, ponder=False)
 
     engine1.start_engine()
     engine2.start_engine()
 
     game = Game(player1=player1, player2=player2, variant=mode)
 
-    print_state(game)
+    if not args.silent:
+        print_state(game)
 
     try:
         while True:
-            str_move = engine1.best_move(str(game.variant), wtime=5000, btime=5000)
+            str_move = engine1.best_move(str(game.variant), wtime=100, btime=100)
             move = StandardMove.from_str(str_move)
             game.move(move)
-            print_state(game)
-            if game.variant.game_state:
+            if not args.silent:
+                print_state(game)
+            if game.variant.game_state[0]:
                 break
 
-            str_move = engine2.best_move(str(game.variant), wtime=1000, btime=1000)
+            str_move = engine2.best_move(str(game.variant), wtime=100, btime=100)
             move = StandardMove.from_str(str_move)
             game.move(move)
-            print_state(game)
-            if game.variant.game_state:
+            if not args.silent:
+                print_state(game)
+            if game.variant.game_state[0]:
                 break
-        print('Winner(s):', ','.join(str(side) for side in game.variant.game_state))
-        print('moves:', [str(move) for move in game.variant.moves_history])
+        if args.silent:
+            print_state(game)
+        print('State: {state}, Winner(s): {winner}'.format(
+            state=game.variant.game_state[1],
+            winner=','.join(str(side) for side in game.variant.game_state[0]))
+        )
+        if not args.silent:
+            print('moves:', [str(move) for move in game.variant.moves_history])
 
         engine1.stop_engine()
         engine2.stop_engine()

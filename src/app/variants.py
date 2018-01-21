a5138179addd4ee796c630df0e04b89a37664f3b
gethiox <gethiox@gmail.com>
2018-01-22 00:56:37 +0100

    added some documentation

--- src/app/variants.py
+++ src/app/variants.py
@@ -39,42 +39,101 @@ class Normal(Variant):
         self.__pocket: Dict[Type['Side'], List[Type['Piece']]] = {side: [] for side in self.sides}
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """
+        return Variant name
+        """
         return "Classic Chess"
 
     @property
-    def en_passant(self):
+    def en_passant(self) -> Optional[StandardPosition]:
+        """
+        return en passant position
+        """
         return self.__en_passant
 
     @property
-    def half_moves(self):
+    def half_moves(self) -> int:
+        """
+        return half-move counter value, starts from 1 and increments with every side move
+        """
         return self.__half_moves
 
     @property
-    def moves(self):
+    def moves(self) -> int:
+        """
+        return full-move counter value, starts from 1 and increments with every first player move (white in that case)
+        """
         return (self.__half_moves + 1) // len(self.sides)
 
     @property
     def moves_history(self) -> List['StandardMove']:
+        """
+        return list of executed moves during the game
+        """
         return self.__moves_history
 
     @property
+    def on_move(self) -> Type['Side']:
+        """
+        return side which is actually on the move
+        """
+        return self.sides[(self.__half_moves - 1) % len(self.sides)]
+
+    @property
+    def pocket(self) -> Dict[Type['Side'], List[Type['Piece']]]:
+        """
+        return dict where key is a side and value is a list of captured pieces by that side
+        """
+        return self.__pocket
+
+    @property
+    def board(self) -> StandardBoard:
+        """
+        return board object which are holding pieces positions
+        """
+        return self.__board
+
+    @property
+    def sides(self) -> List[Type['Side']]:
+        """
+        return ordered list of sides, order of list means order of executing moves in the game
+        """
+        return [White, Black]
+
+    @property
+    def pieces(self) -> Set[Type['Piece']]:
+        # TODO: not used, to remove or something
+        return {King, Queen, Rook, Bishop, Knight, Pawn}
+
+    @property
+    def is_check(self) -> bool:
+        """
+        returns if current, on move player is in check
+        """
+        # TODO: return all pieces and positions which causes check
+        pos, king = self.board.find_pieces(King(self.on_move))[0]
+        if pos in self.attacked_fields_by_sides(set(self.sides) - {self.on_move}):
+            return True
+        return False
+
+    @property
     def last_move(self) -> Optional['StandardMove']:
+        """
+        return last executed move from move history
+        """
+        # TODO: useless when full history can be read directly?
         try:
             return self.moves_history[-1]
         except IndexError:
             return None
 
     @property
-    def on_move(self) -> Type['Side']:
-        return self.sides[(self.__half_moves - 1) % len(self.sides)]
-
-    @property
-    def pocket(self):
-        return self.__pocket
-
-    @property
     def game_state(self) -> Tuple[Optional[Set[Type['Side']]], Optional[str]]:
+        """
+        return tuple where first element is an optional set of winning sides (if is there more than one that meanse
+        these players get a draw). and second element is an optional verdict explanation
+        """
         pieces = {piece for _, piece in self.board.pieces}
         if len(pieces) < 4:
             if pieces == {King(White), King(Black)}:
@@ -101,27 +160,10 @@ class Normal(Variant):
                 return set(self.sides), 'stalemate'
         return None, None
 
-    @property
-    def is_check(self):
-        pos, king = self.board.find_pieces(King(self.on_move))[0]
-        if pos in self.attacked_fields_by_sides(set(self.sides) - {self.on_move}):
-            return True
-        return False
-
-    @property
-    def board(self) -> StandardBoard:
-        return self.__board
-
-    @property
-    def sides(self) -> List[Type['Side']]:
-        return [White, Black]
-
-    @property
-    def pieces(self) -> Set[Type['Piece']]:
-        # TODO: not used, to remove or something
-        return {King, Queen, Rook, Bishop, Knight, Pawn}
-
     def init_board_state(self):
+        """
+        Set board start position for classic chess variant
+        """
         self.board.put_piece(piece=Rook(White), position=StandardPosition((0, 0)))
         self.board.put_piece(piece=Rook(White), position=StandardPosition((7, 0)))
         self.board.put_piece(piece=Knight(White), position=StandardPosition((1, 0)))
@@ -149,20 +191,26 @@ class Normal(Variant):
         return self.board.get_fen()
 
     def assert_move(self, move: 'StandardMove'):
+        """
+        verify if given move is valid in current game state, proper exception is raised when needed
+        """
         source, destination = move.source, move.destination
         piece = self.board.get_piece(source)
         if not piece:
             raise NoPiece("Any piece on %s, you need to move pieces, not an air." % source)
+        # check if requested destination field is in available fields generated by game logic
         available_dest = self.standard_moves(source) | self.standard_captures(source) | self.special_moves(source)
         if destination not in available_dest:
             raise NotAValidMove("%s is not a proper move for a %s %s\npositions available for that piece: %s" % (
                 move, piece.side, piece.name, ', '.join({str(pos) for pos in available_dest})))
+        # create test board for real validations
         test_board = deepcopy(self.board)
         test_piece = test_board.remove_piece(source)  # TODO: test special moves
         test_board.put_piece(test_piece, destination)
-        if isinstance(test_piece, Pawn):
+        if isinstance(test_piece, Pawn):  # TODO: here is an example of above TODO
             if destination == self.en_passant:
                 test_board.remove_piece(StandardPosition((destination.file, source.rank)))
+        # test on the copied board if move not causing any self-check
         king_pos, king = test_board.find_pieces(requested_piece=King(self.on_move))[0]
         if king_pos in self.attacked_fields_by_sides(set(self.sides) - {piece.side}, test_board):
             raise CausesCheck("{move} move causes {side} {name} ({pos}) check delivered by: [{atck}]".format(
@@ -173,46 +221,53 @@ class Normal(Variant):
                      in self.who_can_step_here(king_pos, test_board).items()]
                 )
             ))
+        # simple validation when promotion was declared for promoted pawn
         if isinstance(piece, Pawn) and destination.rank in (7, 0):
             if not move.promotion:
                 raise NotAValidPromotion("Proper promotion are required when promoting a pawn")
 
     def move(self, move: 'StandardMove') -> Optional['Piece']:
+        """
+        try to execute given move, return captured piece if not fails
+        """
         self.assert_move(move)
         source, destination = move.source, move.destination
         moved_piece = self.board.get_piece(position=source)
         if moved_piece.side != self.on_move:
             raise WrongMoveOrder("You are trying to move %s when %s are on move" % (moved_piece.side, self.on_move))
+        # move are accepted by the game logic, all below concerns game state (counters, history, proper move execution)
         self.save_history()
         moved_piece = self.board.remove_piece(position=source)
         taken_piece = self.board.put_piece(piece=moved_piece, position=destination)
         if isinstance(moved_piece, Pawn):
-            if destination == self.en_passant:
+            if destination == self.en_passant:  # check if en passant move was involved
                 taken_piece = self.board.remove_piece(StandardPosition((destination.file, source.rank)))
 
+            # check if pawn was pushed by two fields, set needed en passant position if so
             if abs(source.rank - destination.rank) == 2:
                 self.__en_passant = StandardPosition(
                     (source.file,
                      int((source.rank + destination.rank) / 2))
                 )
-            else:
+            else:  # clear en passant position on any other pawn-move
                 self.__en_passant = None
             self.__half_moves_since_pawn_moved = 0
 
-            if destination.rank in (7, 0):
+            if destination.rank in (7, 0):  # handle promotion
                 self.board.put_piece(move.promotion(self.on_move), destination)
 
-        else:
+        else:  # clear en passant position on any other piece-move
             if self.__en_passant:
                 self.__en_passant = None
             self.__half_moves_since_pawn_moved += 1
 
+        # simple and ugly check for castling execution
         if isinstance(moved_piece, King) and abs(source.file - destination.file) == 2:
             rank = source.rank
-            if move.destination.file == 6:
+            if move.destination.file == 6:  # king-castle
                 moved_rook = self.board.remove_piece(position=StandardPosition((7, rank)))
                 self.board.put_piece(moved_rook, StandardPosition((5, rank)))
-            elif destination.file == 2:
+            elif destination.file == 2:  # queen-castle
                 moved_rook = self.board.remove_piece(position=StandardPosition((0, rank)))
                 self.board.put_piece(moved_rook, StandardPosition((3, rank)))
 
@@ -220,7 +275,7 @@ class Normal(Variant):
             self.__half_moves_since_capture += 1
         else:
             self.__half_moves_since_capture = 0
-            self.__pocket[self.on_move].append(taken_piece)
+            self.__pocket[self.on_move].append(taken_piece)  # put taken piece to the side pocket!
 
         self.__update_castling_info(source, destination)
         self.__position_occurence[hash(self.board)] += 1
@@ -230,6 +285,9 @@ class Normal(Variant):
         return taken_piece
 
     def __update_castling_info(self, source, destination):
+        """
+        simply removes castling ability if given move do so
+        """
         if self.__castling:
             if StandardPosition.from_str('e1') in (source,):
                 self.__castling = self.__castling - {King(White), Queen(White)}
@@ -246,6 +304,9 @@ class Normal(Variant):
                 self.__castling = self.__castling - {King(Black)}
 
     def save_history(self):
+        """
+        save complete variant state into history object
+        """
         self.__board_history.append(deepcopy(
             (
                 self.__board,
@@ -259,7 +320,9 @@ class Normal(Variant):
         ))
 
     def load_history(self, offset: int):
-        """Back in time and recover game state from the past"""
+        """
+        Back in time and recover game state from the past
+        """
         index = self.half_moves - offset - 1
         self.__board, \
         self.__half_moves, \
@@ -273,6 +336,9 @@ class Normal(Variant):
             self.__board_history.pop()
 
     def standard_moves(self, position: 'StandardPosition', board: StandardBoard = None) -> Set['StandardPosition']:
+        """
+        return set of available moves
+        """
         if not board:
             board = self.board
         piece = board.get_piece(position)
@@ -303,6 +369,9 @@ class Normal(Variant):
         return new_positions
 
     def special_moves(self, position: 'StandardPosition', board: StandardBoard = None) -> Set['StandardPosition']:
+        """
+        return set of available special moves (castling and en passant)
+        """
         # TODO: replace for some more convenient solution
         if not board:
             board = self.board
@@ -349,6 +418,9 @@ class Normal(Variant):
         return new_positions
 
     def standard_captures(self, position: 'StandardPosition', board: StandardBoard = None) -> Set['StandardPosition']:
+        """
+        return set of available captures
+        """
         if not board:
             board = self.board
         piece = board.get_piece(position)
@@ -388,6 +460,9 @@ class Normal(Variant):
         return new_positions
 
     def attacked_fields(self, position: 'StandardPosition', board: StandardBoard = None) -> Set['StandardPosition']:
+        """
+        return set of attacked positions which as coming from given position (piece on that position)
+        """
         if not board:
             board = self.board
         piece = board.get_piece(position)
@@ -423,6 +498,9 @@ class Normal(Variant):
 
     def attacked_fields_by_sides(self, sides: Set[Type['Side']], board: 'StandardBoard' = None) \
             -> Set['StandardPosition']:
+        """
+        return attacked positions by the given set of sieds
+        """
         if not board:
             board = self.board
 
@@ -432,13 +510,18 @@ class Normal(Variant):
 
     def who_can_step_here(self, position: 'StandardPosition', board: 'StandardBoard' = None) \
             -> Dict['StandardPosition', 'Piece']:
+        """
+        return dict where key is a position of piece and value is that piece which can move/capture on given position
+        """
         if not board:
             board = self.board
 
         return {pos: piece for pos, piece in board.pieces if position in self.attacked_fields(pos, board)}
 
     def all_available_moves(self, side: Type['Side'] = None) -> List[StandardMove]:
-        # Warning! Very inefficient!
+        """
+        return list of all available and validated moves by the current "on move" side, very inefficient.
+        """
         if not side:
             side = self.on_move
 
@@ -458,6 +541,9 @@ class Normal(Variant):
 
     @staticmethod
     def __transform_vector(vector: Tuple[int, int], all_directions: bool, side) -> Set[Tuple[int, int]]:
+        """
+        return set of vector variants to specify pieces and sides move directions
+        """
         # TODO: standard interface for resolving all vector combinations and for transforming not-all-directions vector
         # TODO: eg. a pawn, for Whites move forward means incrementing rank value, for Black - decrementing
         if all_directions:
@@ -478,7 +564,10 @@ class Normal(Variant):
             return {vector}
 
     def can_i_make_a_move(self) -> bool:
-        # improvement to check if piece that causes check can be captured
+        """
+        return True if some move is available, False if not (duh)
+        """
+        # TODO: improvement to check if piece that causes check can be captured
         for pos, piece in self.board.pieces:
             if piece and piece.side == self.on_move:
                 for moves in [self.standard_captures(pos), self.standard_moves(pos), self.special_moves(pos)]:
@@ -491,6 +580,9 @@ class Normal(Variant):
         return False
 
     def fen(self):
+        """
+        return FEN game-state notation
+        """
         return str(self)
 
     def __hash__(self):
